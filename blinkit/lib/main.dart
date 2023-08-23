34e0cdd5bb33f8388e36fe7c00690823477d9843
garvit1613 <garvit1613@gmail.com>
2023-08-23 23:50:32 +0530

    files added

---
+++ blinkit/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:blinkit/screens/Authscreen.dart';
+
+import 'package:flutter/material.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthPage(),
+    ),
+  );
+}
